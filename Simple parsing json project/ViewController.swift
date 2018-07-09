d52ab9e569081a094ad1496f98a9f619a7b80e5e
ahmetberkaycalisti <ahmetberkaycalisti@gmail.com>
2018-07-09 11:18:22 +0800

    A project shows that how to simply parse json data

--- Simple parsing json project/ViewController.swift
+++ Simple parsing json project/ViewController.swift
@@ -10,9 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // let path = Bundle.main.path(forResource: "jsonData", ofType: "json")
+        // print(path ?? "not a real path")
+        // have to do some optional chaining because we don't want to optional string. Also we can do force to wrap it but its not a good practice for code
+        guard let path = Bundle.main.path(forResource: "jsonData", ofType: "json") else { return }
+        let url = URL(fileURLWithPath: path)
+        
+        // its gonna show the actual bytes. This isnt readable
+        do {
+            let data = try Data(contentsOf: url)
+            // We can't really work with bytes so I'm going to actually do some serialization of that data. After that we actually know what we're working with
+            let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers)
+            print(data)
+            print(json)
+            
+            // do some optional chaining
+            guard let array = json as? [Any] else { return }
+            
+            for user in array {
+                guard let userDict = user as? [String: Any] else { return }
+                guard let userId = userDict["id"] as? Int else { print("not an Int"); return }
+                guard let name = userDict["name"] as? String else {return}
+                // Most likely we're gonna do "Any" in real world situation but we know exactly what we're getting back
+                guard let company = userDict["company"] as? [String: String] else { return }
+                guard let companyName = company["name"] else {return }
+                
+                
+                print(userId)
+                print(name)
+                print(companyName)
+                print(" ")
+            }
+            
+        } catch  {
+            print(error)
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
