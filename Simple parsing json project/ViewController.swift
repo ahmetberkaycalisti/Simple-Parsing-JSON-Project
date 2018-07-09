3ff4b1e2bd19f266a328f9159b4c5e1c192ea989
ahmetberkaycalisti <ahmetberkaycalisti@gmail.com>
2018-07-09 10:56:38 +0800

    Initial Commit

---
+++ Simple parsing json project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simple parsing json project
+//
+//  Created by Ahmet Berkay CALISTI on 09/07/2018.
+//  Copyright © 2018 Ahmet Berkay CALISTI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
